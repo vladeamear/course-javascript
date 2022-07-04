90a0004683cfafacbd4458a5b95ccab926a4ea85
Владимир Мясин <v.miasin@kokoc.tech>
2022-07-04 11:13:35 +0300

    6-dnd: Выполнено ДЗ №6

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,46 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+document.addEventListener('mousemove', (e) => {
+  const target = [...e.target.classList].includes('draggable-div') && e.target;
+  if (target) {
+    target.onmousedown = () => {
+      target.style.left = e.pageX - target.offsetWidth / 2 + 'px';
+      target.style.top = e.pageY - target.offsetHeight / 2 + 'px';
+      target.onmousemove = (evt) => {
+        target.style.left = evt.pageX - target.offsetWidth / 2 + 'px';
+        target.style.top = evt.pageY - target.offsetHeight / 2 + 'px';
+      };
+    };
+    target.onmouseup = function () {
+      target.onmousemove = null;
+    };
+  }
+});
 
-export function createDiv() {}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export function createDiv() {
+  const div = document.createElement('DIV');
+  div.style.position = 'fixed';
+  const width = getRandomInt(100);
+  const height = getRandomInt(100);
+  div.style.width = `${width > window.innerWidth ? window.innerWidth : width}px`;
+  div.style.height = `${height > window.innerHeight ? window.innerHeight : height}px`;
+  div.style.top = `${getRandomInt(
+    window.innerHeight - (height > window.innerHeight ? window.innerHeight : height)
+  )}px`;
+  div.style.left = `${getRandomInt(
+    window.innerWidth - (width > window.innerWidth ? window.innerWidth : width)
+  )}px`;
+  div.style.backgroundColor = `rgb(${getRandomInt(255)},${getRandomInt(
+    255
+  )},${getRandomInt(255)})`;
+  div.classList.add('draggable-div');
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
